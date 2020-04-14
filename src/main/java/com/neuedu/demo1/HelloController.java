6a8979fe2c480d01b9a8f1b115c08a288bc6a656
g465801892p <465801892@qq.com>
2020-04-14 21:35:10 +0800

    test

--- src/main/java/com/neuedu/demo1/HelloController.java
+++ src/main/java/com/neuedu/demo1/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
     @GetMapping("/hello")
     public String hello(){
-
+                int i = 0 ;
         return "{\"content\":\"hello Spring Boot!!\"}";
     }
 
